99f1cb8d35f4b76e7c00d603f8761f04c2e404f5
BlinSahatqija <bs45605@ubt-uni.net>
2021-04-12 21:55:37 +0200

    footeri

---
+++ projektivue/src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="footer">
+    <hr>
+    <footer class="page-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-8 col-sm-12">
+            <h6 class="text-uppercase font-weight-bold">Additional Information</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <h6 class="text-uppercase font-weight-bold">Contact</h6>
+            <p>Lagjia Kalabria p.n
+            <br/>info@mywebsite.com
+            <br/>+ 383 38 541 400
+            <br/>+ 383 44 541 400</p>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright text-center">© 2020 Copyright: MyWebsite.com</div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.footer{
+  background-color: #17a2b8 !important; 
+  color:rgba(255, 255, 255, 0.6); 
+  margin-top:60px
+}
+</style>
