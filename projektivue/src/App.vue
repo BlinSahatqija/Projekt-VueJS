5d9d70932d38c3b37690e0b4f610755acbc8d744
Genat Hadri <gh43836@ubt-uni.net>
2021-04-16 16:57:01 +0200

    firebase file

--- projektivue/src/App.vue
+++ projektivue/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-<Header></Header>
-<Footer></Footer>
+    <Header/>
+      <router-view></router-view>
+    <Footer/>
   </div>
 </template>
 
 <script>
+//import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+
 export default {
   name: 'App',
   components: {
-    Header, Footer
+     Header, Footer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>  
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0px;
+  }
 </style>
---
+++ projektivue/src/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import store from './store';
+
+const settings = {timestampsInSnapshots:true};
+
+const config = {
+    apiKey: "AIzaSyBHjAUTvTLK_jSgMKYiDUEOFefJ2c651cY",
+    authDomain: "projektiexample.firebaseapp.com",
+    databaseURL: "https://projektiexample-default-rtdb.firebaseio.com",
+    projectId: "projektiexample",
+    storageBucket: "projektiexample.appspot.com",
+    messagingSenderId: "973407722910",
+    appId: "1:973407722910:web:c2b1080ab61fb258058a8f",
+    measurementId: "G-CXKG7QEBGV"
+};
