de6c280a4524b6301cb8ba07cda3c46ddabeb883
titan shala <ts44423@ubt-uni.net>
2021-04-21 19:31:48 +0200

    Register

---
+++ projektivue/src/components/auth/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+        <div class="card">
+          <div class="card-header"><h3>Register Form</h3></div>
+          <div class="card-body">
+            <div v-if="error" class="alert alert-danger">{{error}}</div>
+            <form action="#" @submit.prevent="submit">            
+              <div class="imgcontainer">
+                <img :src="avatar" alt="Avatar" class="avatar">
+              </div>
+                <div class="form-group row">
+                <label for="name"><b>Name</b></label>
+                  <input id="name"
+                  type="text"
+                  name="name"
+                  value
+                  required
+                  autofocus
+                  >
+
+                  <label for="email"><b>Email</b></label>
+                  <input id="email"
+                  type="email"
+                  name="email"
+                  value
+                  required
+                  autofocus
+                  >
+
+                  <label for="password"><b>Password</b></label>
+                  <input id="password"
+                  type="password"
+                  name="password"
+                  required
+                  >
+
+                  <button type="submit">Register</button>
+                </div>            
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
