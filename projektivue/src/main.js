52e565f24cf1261fcb2609d15e8b49a049eccc83
Genat Hadri <gh43836@ubt-uni.net>
2021-04-22 12:02:39 +0200

    main file ndryshime

--- projektivue/src/main.js
+++ projektivue/src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
+import VueRouter from 'vue-router'
+//fajlli routes importohet te kjo pjese dhe ne at file kemi deklaruar routat e ndryshem se cila route te cila komponent dergon gjate klikut
+import router from './router'
+import store from './store'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(VueRouter);
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
@@ -15,5 +19,7 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
