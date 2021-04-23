eccaf831c98d930bbfd110a3b230721809700983
Genat Hadri <gh43836@ubt-uni.net>
2021-04-23 20:10:48 +0200

    contact.js tek routes

---
+++ projektivue/routes/contacts.js
@@ -0,0 +1,50 @@
+var Contacts = require('../models/Contacts');
+var express = require('express');
+var mongoose = require('mongoose');
+var router = express.Router();
+
+//CRUD Routes
+
+//GET ALL DATA
+router.get('/', function(req, res, next){
+    Contacts.find(function(err, contacts){
+        if(err) return next(err);
+        res.json(contacts);
+    });
+});
+
+//GET single DATA by ID
+router.get('/:id', function(req, res, next){
+    Contacts.findById(req.params.id, function(err, post){
+        if(err) return next(err);
+        res.json(post);
+    });
+});
+
+//POST CREATE DATA
+router.post('/', function(req, res, next) {
+    Contacts.create(req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+//UPDATE DATA
+router.put('/:id', function(req, res, next) {
+    console.log(req.body);
+    Contacts.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+//DELETE DATA
+router.delete('/:id', function(req, res, next) {
+    console.log(req.body);
+    Contacts.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+module.exports = router;
