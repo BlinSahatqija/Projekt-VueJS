b89fff5063f1f4a89f2d8aed1954e3045a5d2d3c
BlinSahatqija <bs45605@ubt-uni.net>
2021-04-27 20:59:57 +0200

    ndryshime server.js

--- projektivue/config/server.js
+++ projektivue/config/server.js
@@ -10,3 +10,14 @@ const jobs = require('../routes/jobs');
 mongoose.Promise = global.Promise;  
 mongoose.connect(config.DB);       
 
+const app = express(); // tu e nis ni instance te expressit, per mi leshu serverin
+app.use(bodyParser.json()); //i bojim te dhenat e gatshme mu perdor me json
+app.use(cors());
+app.use('/jobs', jobs);
+app.use('/contacts', contacts);  //pathi contacts
+var port = process.env.PORT || 4000;
+
+app.listen(port, function(){
+    console.log("NodeJS Serveri online:", port);
+});
+
